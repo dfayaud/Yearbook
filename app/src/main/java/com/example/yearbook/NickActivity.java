7840d70840687fdd360bfba9f4ac6c23ea84c12a
Coda759 <45927826+Coda759@users.noreply.github.com>
2021-02-18 14:24:58 -0500

    Nick's Page Done!

--- app/src/main/java/com/example/yearbook/NickActivity.java
+++ app/src/main/java/com/example/yearbook/NickActivity.java
@@ -2,7 +2,9 @@ package com.example.yearbook;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class NickActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class NickActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nick);
+    }
+
+    public void switchDannyActivity(View view){
+        Intent intent = new Intent(this, DannyActivity.class);
+        startActivity(intent);
+    }
+
+    public void switchJohnActivity(View view){
+        Intent intent = new Intent(this, JohnActivity.class);
+        startActivity(intent);
     }
 }
