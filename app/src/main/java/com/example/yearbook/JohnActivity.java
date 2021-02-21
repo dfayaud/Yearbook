1e264bda31152fa3b5b01c8774df265703cb2412
Ross Rando <john.gedell@gmail.com>
2021-02-20 21:23:47 -0500

    John commit

--- app/src/main/java/com/example/yearbook/JohnActivity.java
+++ app/src/main/java/com/example/yearbook/JohnActivity.java
@@ -2,7 +2,9 @@ package com.example.yearbook;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class JohnActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class JohnActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_john);
+    }
+
+    public void switchNickActivity(View view){
+        Intent intent = new Intent(this, NickActivity.class);
+        startActivity(intent);
+    }
+
+    public void switchDannyActivity(View view){
+        Intent intent = new Intent(this, JohnActivity.class);
+        startActivity(intent);
     }
 }
