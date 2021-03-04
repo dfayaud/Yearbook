c29e5605fd63758cb2bb4b74de4824c987d85481
Coda759 <45927826+Coda759@users.noreply.github.com>
2021-03-04 17:46:27 -0500

    Notification pop up done

--- app/src/main/java/com/example/yearbook/MainActivity.java
+++ app/src/main/java/com/example/yearbook/MainActivity.java
@@ -1,9 +1,16 @@
 package com.example.yearbook;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +19,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //To run on Oreo or greater, create notification channel apparently
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel("idle notification", "inote", NotificationManager.IMPORTANCE_HIGH);
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
     }
 
     public void switchNickActivity(View view){
@@ -28,4 +42,40 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, JohnActivity.class);
         startActivity(intent);
     }
+
+    public static String getApplicationName(Context context) {
+        ApplicationInfo applicationInfo = context.getApplicationInfo();
+        int stringId = applicationInfo.labelRes;
+        return stringId == 0 ? applicationInfo.nonLocalizedLabel.toString() : context.getString(stringId);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        new CountDownTimer(5000, 1000) {
+
+            @Override
+            public void onTick(long l) {
+
+            }
+
+            public void onFinish() {
+                displayNotification();
+            }
+        }.start();
+    }
+
+    public void displayNotification() {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "idle notification");
+        builder.setContentTitle("App Running");
+        builder.setContentText("App " + getApplicationName(this) + "is still running");
+        builder.setSmallIcon(R.drawable.ic_launcher_background);
+        builder.setPriority(NotificationCompat.PRIORITY_MAX);
+        builder.setAutoCancel(true);
+
+        NotificationManagerCompat managerCompat = NotificationManagerCompat.from(this);
+        managerCompat.notify(1, builder.build());
+    }
+
 }
